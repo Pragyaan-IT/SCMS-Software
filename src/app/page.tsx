19d86d566e2476a9659ef7f1fe5ec37204a455f5
Aman Varshney <av.amanvarshney11@gmail.com>
2024-08-24 19:50:50 +0530

    remove useless code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,12 @@ export default async function Home() {
       <Button asChild>
         <Link href="/login">Login</Link>
       </Button>
+      <Button asChild>
+        <Link href="/student/dashboard">Student Dashboard</Link>
+      </Button>
+      <Button asChild>
+        <Link href="/teacher/dashboard">Teacher Dashboard</Link>
+      </Button>
     </main>
   );
 }
