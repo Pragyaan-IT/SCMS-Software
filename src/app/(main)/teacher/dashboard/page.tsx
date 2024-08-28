03fdf0a93522041e6907655abd289d9f7dbb44f6
Aman Varshney <av.amanvarshney11@gmail.com>
2024-08-28 19:38:50 +0530

    add charts for teacher dashboard

--- src/app/(main)/teacher/dashboard/page.tsx
+++ src/app/(main)/teacher/dashboard/page.tsx
@@ -3,11 +3,10 @@ import PageTitle from "@/components/page-title";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { AttendanceRateChart } from "../_components/attendance-rate-chart";
+import ProfileScore from "../_components/profile-score";
 import StudentPerformanceChart from "../_components/student-performance-chart";
-import { useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { SubjectPerformanceChart } from "../_components/subject-perfomance-chart";
+import QuestionsAnswered from "../_components/questions-answered";
 
 export default function DashboardPage() {
   // const { data: session } = useSession();
@@ -19,12 +18,14 @@ export default function DashboardPage() {
   //   if (session && session.user.role !== "teacher") {
   //     router.push("/");
   //   }
-    
+
   // }, [session]);
   return (
     <section>
       <PageTitle title="Dashboard" />
       <ScrollArea className="h-[calc(100dvh-140px)] w-full">
+        <ProfileScore />
+        <QuestionsAnswered />
         <div className="grid grid-cols-1 gap-4 p-2 md:grid-cols-2 lg:grid-cols-3">
           <StudentPerformanceChart />
           <AttendanceRateChart />
