4effa54952fd770c171db27a8dd93a31becc06b6
Aman Varshney <av.amanvarshney11@gmail.com>
2024-08-29 19:58:23 +0530

    updateeeeeeeee

--- src/app/(main)/student/dashboard/profile/page.tsx
+++ src/app/(main)/student/dashboard/profile/page.tsx
@@ -1,110 +1,110 @@
-"use client"
+"use client";
 
-import { useAppContext } from "@/components/providers/context-provider"
+import { useAppContext } from "@/components/providers/context-provider";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Student } from "@/lib/types";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Separator } from "@/components/ui/separator";
+import { useEffect, useState } from "react";
 
 export default function ProfilePage() {
-    const { getStudentProfile } = useAppContext()
-    const { data: session } = useSession();
-    const [profile, setProfile] = useState<Student[]>([])
+  const { getStudentProfile } = useAppContext();
+  const { data: session } = useSession();
+  const [profile, setProfile] = useState<Student[]>([]);
 
-    useEffect(() => {
-        async function getProfile(id: string) {
-            const profileData = await getStudentProfile(id);
-            setProfile(profileData);
-        }
-        if (session) {
-            getProfile(session?.user.id);
-        }
-    }, [session])
+  useEffect(() => {
+    async function getProfile(id: string) {
+      const profileData = await getStudentProfile(id);
+      setProfile(profileData);
+    }
+    if (session) {
+      getProfile(session?.user.id);
+    }
+  }, [session]);
 
-    return (
-
-        <Tabs defaultValue="profile" className="py-8 px-16">
-            <div>
-                <nav className="flex gap-4">
-                    <TabsList className="bg-transparent">
-                        <TabsTrigger value="profile" className="data-[state=active]:border-b-1 data-[state=active]:border-black data-[state=active]:shadow-none rounded-none" >
-                            <span className="text-lg font-medium text-gray-900 hover:border-gray-400 hover:text-gray-800"> Profile </span>
-                        </TabsTrigger>
-                        <TabsTrigger value="progress" className="data-[state=active]:border-b-1 data-[state=active]:border-black data-[state=active]:shadow-none rounded-none">
-                            <span className="inline-flex items-center font-medium text-gray-600 text-lg">
-                                Progress Report
-                            </span>
-                        </TabsTrigger>
-                    </TabsList>
-                </nav>
+  return (
+    <Tabs defaultValue="profile" className="px-16 py-8">
+      <div>
+        <nav className="flex gap-4">
+          <TabsList className="bg-transparent">
+            <TabsTrigger
+              value="profile"
+              className="rounded-none data-[state=active]:border-b-1 data-[state=active]:border-black data-[state=active]:shadow-none"
+            >
+              <span className="text-lg font-medium">Profile</span>
+            </TabsTrigger>
+            <TabsTrigger
+              value="progress"
+              className="rounded-none data-[state=active]:border-b-1 data-[state=active]:border-black data-[state=active]:shadow-none"
+            >
+              <span className="inline-flex items-center text-lg font-medium">
+                Progress Report
+              </span>
+            </TabsTrigger>
+          </TabsList>
+        </nav>
+      </div>
+      <Separator className="w-2/4" />
+      <TabsContent value="profile">
+        <div className="mt-6 flex h-4/5 flex-col items-center justify-center">
+          <div className="shadow-3xl shadow-shadow-500 dark:!bg-navy-800 relative mx-auto flex w-[700px] max-w-[95%] flex-col items-center rounded-[20px] bg-clip-border p-3 dark:!shadow-none">
+            <div className="mb-8 mt-2 w-full">
+              <h4 className="px-2 text-xl font-bold">{profile[0]?.name}</h4>
+              <p className="mt-2 px-2 text-base">
+                As we live, our hearts turn colder. Cause pain is what we go
+                through as we become older. We get insulted by others, lose
+                trust for those others. We get back stabbed by friends. It
+                becomes harder for us to give others a hand. We get our heart
+                broken by people we love, even that we give them all...
+              </p>
             </div>
-            <Separator className="w-2/4 bg-gray-400" />
-            <TabsContent value="profile">
-                <div className="flex flex-col justify-center items-center h-4/5 mt-6">
-                    <div className="relative flex flex-col items-center rounded-[20px] w-[700px] max-w-[95%] mx-auto bg-gray-200 bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:!shadow-none p-3">
-                        <div className="mt-2 mb-8 w-full">
-                            <h4 className="px-2 text-xl font-bold text-navy-700 dark:text-white">
-                                {profile[0]?.name}
-                            </h4>
-                            <p className="mt-2 px-2 text-base text-gray-600">
-                                As we live, our hearts turn colder. Cause pain is what we go through
-                                as we become older. We get insulted by others, lose trust for those
-                                others. We get back stabbed by friends. It becomes harder for us to
-                                give others a hand. We get our heart broken by people we love, even
-                                that we give them all...
-                            </p>
-                        </div>
-                        <div className="grid grid-cols-2 gap-4 px-2 w-full">
-                            <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p className="text-sm text-gray-600">Class</p>
-                                <p className="text-base font-medium text-navy-700 dark:text-white">
-                                    Stanford University
-                                </p>
-                            </div>
+            <div className="grid w-full grid-cols-2 gap-4 px-2">
+              <div className="shadow-3xl shadow-shadow-500 flex flex-col items-start justify-center rounded-2xl bg-clip-border px-3 py-4 dark:shadow-none">
+                <p className="text-sm">Class</p>
+                <p className="text-navy-700 text-base font-medium">
+                  Stanford University
+                </p>
+              </div>
 
-                            <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p className="text-sm text-gray-600">Languages</p>
-                                <p className="text-base font-medium text-navy-700 dark:text-white">
-                                    English, Spanish, Italian
-                                </p>
-                            </div>
+              <div className="shadow-3xl shadow-shadow-500 flex flex-col justify-center rounded-2xl bg-clip-border px-3 py-4 dark:shadow-none">
+                <p className="text-sm">Languages</p>
+                <p className="text-navy-700 text-base font-medium">
+                  English, Spanish, Italian
+                </p>
+              </div>
 
-                            <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p className="text-sm text-gray-600">Department</p>
-                                <p className="text-base font-medium text-navy-700 dark:text-white">
-                                    Product Design
-                                </p>
-                            </div>
+              <div className="shadow-3xl shadow-shadow-500 flex flex-col items-start justify-center rounded-2xl bg-clip-border px-3 py-4 dark:shadow-none">
+                <p className="text-sm">Department</p>
+                <p className="text-navy-700 text-base font-medium">
+                  Product Design
+                </p>
+              </div>
 
-                            <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p className="text-sm text-gray-600">Work History</p>
-                                <p className="text-base font-medium text-navy-700 dark:text-white">
-                                    English, Spanish, Italian
-                                </p>
-                            </div>
+              <div className="shadow-3xl shadow-shadow-500 flex flex-col justify-center rounded-2xl bg-clip-border px-3 py-4 dark:shadow-none">
+                <p className="text-sm">Work History</p>
+                <p className="text-navy-700 text-base font-medium">
+                  English, Spanish, Italian
+                </p>
+              </div>
 
-                            <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p className="text-sm text-gray-600">Organization</p>
-                                <p className="text-base font-medium text-navy-700 dark:text-white">
-                                    Simmmple Web LLC
-                                </p>
-                            </div>
+              <div className="shadow-3xl shadow-shadow-500 flex flex-col items-start justify-center rounded-2xl bg-clip-border px-3 py-4 dark:shadow-none">
+                <p className="text-sm">Organization</p>
+                <p className="text-navy-700 text-base font-medium">
+                  Simmmple Web LLC
+                </p>
+              </div>
 
-                            <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p className="text-sm text-gray-600">Birthday</p>
-                                <p className="text-base font-medium text-navy-700 dark:text-white">
-                                    20 July 1986
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </TabsContent>
-            <TabsContent value="progress">
-            </TabsContent>
-        </Tabs>
-
-    )
+              <div className="shadow-3xl shadow-shadow-500 flex flex-col justify-center rounded-2xl bg-clip-border px-3 py-4 dark:shadow-none">
+                <p className="text-sm">Birthday</p>
+                <p className="text-navy-700 text-base font-medium">
+                  20 July 1986
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </TabsContent>
+      <TabsContent value="progress"></TabsContent>
+    </Tabs>
+  );
 }
--- src/app/(main)/student/dashboard/timetable/page.tsx
+++ src/app/(main)/student/dashboard/timetable/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useAppContext } from "@/components/providers/context-provider";
 import { getTiming } from "@/lib/getTiming";
@@ -8,119 +8,160 @@ import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 export default function TimetablePage() {
-    const { getTodayClass } = useAppContext();
+  const { getTodayClass } = useAppContext();
 
-    const { data: session } = useSession();
-    const [todayClass, setTodayClass] = useState<TodayClass[] | null>(null);
-    const [nextFiveDays, setNextFiveDays] = useState<any[]>([])
-    const [nextFiveDates, setNextFiveDates] = useState<any[]>([])
-    const [activeDate, setActiveDate] = useState("")
+  const { data: session } = useSession();
+  const [todayClass, setTodayClass] = useState<TodayClass[] | null>(null);
+  const [nextFiveDays, setNextFiveDays] = useState<any[]>([]);
+  const [nextFiveDates, setNextFiveDates] = useState<any[]>([]);
+  const [activeDate, setActiveDate] = useState("");
 
-    const getDays = () => {
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const today = new Date();
-        const result = [];
-        for (let i = 0; i < 7; i++) {
-            const nextDay = new Date(today);
-            nextDay.setDate(today.getDate() + i);
-            if (nextDay.getDay() !== 0 && nextDay.getDay() !== 6) {
-                result.push(days[nextDay.getDay()]);
-            }
-        }
-        setNextFiveDays(result);
+  const getDays = () => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const today = new Date();
+    const result = [];
+    for (let i = 0; i < 7; i++) {
+      const nextDay = new Date(today);
+      nextDay.setDate(today.getDate() + i);
+      if (nextDay.getDay() !== 0 && nextDay.getDay() !== 6) {
+        result.push(days[nextDay.getDay()]);
+      }
+    }
+    setNextFiveDays(result);
+  };
+
+  const getDates = () => {
+    const dates = [];
+    const today = new Date();
+    for (let i = 0; i < 7; i++) {
+      const nextDay = new Date(today);
+      nextDay.setDate(today.getDate() + i);
+      if (nextDay.getDay() !== 0 && nextDay.getDay() !== 6) {
+        dates.push(nextDay.getDate());
+      }
+    }
+    setNextFiveDates(dates);
+  };
+
+  useEffect(() => {
+    async function getTodayStudentClass(id: string) {
+      const today = new Date().toLocaleString("en-US", { weekday: "long" });
+      const classes = await getTodayClass(id, today);
+      setTodayClass(classes);
     }
 
-    const getDates = () => {
-        const dates = [];
-        const today = new Date();
-        for (let i = 0; i < 7; i++) {
-            const nextDay = new Date(today);
-            nextDay.setDate(today.getDate() + i);
-            if (nextDay.getDay() !== 0 && nextDay.getDay() !== 6) {
-                dates.push(nextDay.getDate());
-            }
-        }
-        setNextFiveDates(dates);
+    if (session) {
+      getTodayStudentClass(session?.user.id);
     }
+    getDays();
+    getDates();
+  }, [session]);
 
-    useEffect(() => {
-        async function getTodayStudentClass(id: string) {
-            const today = new Date().toLocaleString('en-US', { weekday: 'long' });
-            const classes = await getTodayClass(id, today);
-            setTodayClass(classes);
-        }
-
-        if (session) {
-            getTodayStudentClass(session?.user.id);
-        }
-        getDays();
-        getDates();
-    }, [session])
-
-    const findTimetable = async (index: number) => {
-        setActiveDate(nextFiveDates[index]);
-        const timetable = await getTodayClass(session?.user.id!, nextFiveDays[index]);
-        setTodayClass(timetable);
-    }
-
-    return (
-        <div className="p-4">
-            <h1 className="font-semibold text-2xl text-gray-800">Timetable</h1>
-            <div className="w-full mx-auto">
-                <div className="flex justify-between items-center p-4 mb-4 bg-gray-300 border-b-1 border-gray-200 relative">
-                    <div className="flex items-center">
-                        <div className="text-lg font-semibold mr-4">
-                            <span className="bg-blue-200 text-blue-800 py-1 px-2 rounded-full">26</span>
-                            <span className="ml-2">August</span>, <span className="ml-2">2024</span>
-                        </div>
-                        <button id="toggle-calendar" className="ml-2 text-blue-500 hover:text-blue-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div className="search-bar">
-                        <input type="text" placeholder="Search months" className="search-input border border-gray-300 rounded-lg py-1 px-3" />
-                    </div>
-                </div>
-
-                <div className="flex flex-col">
-                    <div className="grid grid-cols-5 uppercase">
-                        {
-                            nextFiveDays.map((day, index) => (
-                                <div key={index} className="p-4 text-center bg-gray-300 relative font-bold ">{day}</div>
-                            ))
-                        }
-                    </div>
-                    <div className="grid grid-cols-5">
-                        {
-                            nextFiveDates.map((date, index) => (
-                                <div key={index} onClick={() => { findTimetable(index) }} className={cn("p-4 text-center relative bg-gray-400 cursor-pointer", activeDate===date && "bg-gray-200",new Date().getDate() === date && "bg-gray-800 text-white")}>{date}</div>
-                            ))
-                        }
-                    </div>
-                </div>
-
-                <div className="bg-gray-300 p-2 border-t-1 border-gray-300 rounded">
-                    <p className="font-semibold">Holidays</p>
-                    <ul>
-                        <li>August 26 - Janmashtami</li>
-                        <li>August 28 - Sports Day</li>
-                    </ul>
-                </div>
-
-                <div className="bg-gray-300 p-2 border-t-1 border-gray-300 rounded mt-4">
-                    <p className="font-semibold">Timetable: {todayClass?.[0].day}</p>
-                    {
-                        todayClass?.map((item, index) => (
-                            <div key={index} className="flex justify-between items-center p-2 border-b-1 border-gray-400 font-bold">
-                                <span className="class-time">{getTiming(item.slot)}</span>
-                                <span className="class-subject">{item.subjectName}</span>
-                            </div>
-                        ))
-                    }
-                </div>
-            </div>
-        </div>
+  const findTimetable = async (index: number) => {
+    setActiveDate(nextFiveDates[index]);
+    const timetable = await getTodayClass(
+      session?.user.id!,
+      nextFiveDays[index],
     );
+    setTodayClass(timetable);
+  };
+
+  return (
+    <div className="p-4">
+      <h1 className="te text-2xl font-semibold">Timetable</h1>
+      <div className="mx-auto w-full">
+        <div className="relative mb-4 flex items-center justify-between border border-b-1 p-4">
+          <div className="flex items-center">
+            <div className="mr-4 text-lg font-semibold">
+              <span className="rounded-full px-2 py-1 text-blue-800">26</span>
+              <span className="ml-2">August</span>,{" "}
+              <span className="ml-2">2024</span>
+            </div>
+            <button
+              id="toggle-calendar"
+              className="ml-2 text-blue-500 hover:text-blue-700"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search months"
+              className="search-input rounded-lg border px-3 py-1"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col">
+          <div className="grid grid-cols-5 uppercase">
+            {nextFiveDays.map((day, index) => (
+              <div key={index} className="relative p-4 text-center font-bold">
+                {day}
+              </div>
+            ))}
+          </div>
+          <div className="grid grid-cols-5">
+            {nextFiveDates.map((date, index) => (
+              <div
+                key={index}
+                onClick={() => {
+                  findTimetable(index);
+                }}
+                className={cn(
+                  "relative cursor-pointer p-4 text-center",
+                  activeDate === date && "",
+                  new Date().getDate() === date && "text-white",
+                )}
+              >
+                {date}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="rounded border-t-1 p-2">
+          <p className="font-semibold">Holidays</p>
+          <ul>
+            <li>August 26 - Janmashtami</li>
+            <li>August 28 - Sports Day</li>
+          </ul>
+        </div>
+
+        <div className="mt-4 rounded border-t-1 p-2">
+          <p className="font-semibold">Timetable: {todayClass?.[0].day}</p>
+          {todayClass?.map((item, index) => (
+            <div
+              key={index}
+              className="bord flex items-center justify-between border-b-1 p-2 font-bold"
+            >
+              <span className="class-time">{getTiming(item.slot)}</span>
+              <span className="class-subject">{item.subjectName}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
